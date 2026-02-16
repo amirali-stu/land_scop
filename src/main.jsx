885d8a058a8ece4449870a733d815f66864f10a4
amirAlimosolo <amirali.mosolo0@gmail.com>
2026-02-16 16:15:10 +0330

    responsive and design v1 is done

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import Users from "./Users";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+      {/* header */}
       <header>
         <TopHeader />
         <NavHeader />
